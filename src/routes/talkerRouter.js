5268eb383f2d44a861750b21764267fb879a88e6
LeandroDoudement <leandrodoudement@gmail.com>
2023-02-06 16:21:10 -0300

    Requisito 1 | Endpoint GET /talker

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -7,15 +7,14 @@ const router = express.Router();
 const { readTalkerData } = require('../utils/fsTalker');
 
 router.get('/', async (_req, res) => {
-  try {
-    const data = await readTalkerData();
-    if (data.length <= 0) {
-      return res.status(200).json([]);
+    try {
+      const result = await readTalkerData();
+      console.log(result);
+      if (result.length === 0) return res.status(200).json([]);
+      return res.status(200).json(result);
+    } catch (error) {
+      return res.status(500).json(error.message); 
     }
-    return res.status(200).json(data);
-  } catch (error) {
-    return res.status(500).json(error.message);
-  }
-});
+  });
 
 module.exports = router;
--- src/utils/fsTalker.js
+++ src/utils/fsTalker.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 async function readTalkerData() {
   try {
-    const data = await fs.readFile(path.resolve(__dirname, '../files/talker.json'), 'utf-8');
+    const data = await fs.readFile(path.resolve(__dirname, '..', 'talker.json'), 'utf-8');
     return JSON.parse(data);
   } catch (err) {
     console.error(`Erro ao ler o arquivo: ${err.message}`);
